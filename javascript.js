5264e1ec03df69fa92a7288cdd0e5d82af61b260
JSLSasiRekha <105384982+JSLSasiRekha@users.noreply.github.com>
2022-08-21 21:21:32 +0530

    FAQ_Card

---
+++ javascript.js
@@ -0,0 +1,70 @@
+function checksize()
+  {
+    var platform="desktop";
+    var boxDisplay="flex";
+     
+  }
+
+  function myfunction1(){
+    div1=document.getElementById("arrow1")  
+    div12=document.getElementById("arr1")  
+    if(div1.style.display=="none"){
+     div1.style.display="block";
+     div12.style.display="none";
+    }
+    else {
+    div1.style.display = "none";
+    div12.style.display="block";
+  }
+}
+  function myfunction2(){
+    div1=document.getElementById("arrow2")  
+    div12=document.getElementById("arr2")  
+    if(div1.style.display=="none"){
+     div1.style.display="block";
+     div12.style.display="none";
+    }
+    else {
+    div1.style.display = "none";
+    div12.style.display="block";
+    
+  }
+}
+  function myfunction3(){
+    div1=document.getElementById("arrow3")  
+    div12=document.getElementById("arr3")  
+    if(div1.style.display=="none"){
+     div1.style.display="block";
+     div12.style.display="none";
+    }
+    else {
+    div1.style.display = "none";
+    div12.style.display="block";
+    
+  }
+}
+  function myfunction4(){
+    div1=document.getElementById("arrow4")  
+    div12=document.getElementById("arr4")  
+    if(div1.style.display=="none"){
+     div1.style.display="block";
+     div12.style.display="none";
+    }
+    else {
+    div1.style.display = "none";
+    div12.style.display="block";
+  }
+}
+  function myfunction5(){
+    div1=document.getElementById("arrow5")  
+    div12=document.getElementById("arr5")  
+    if(div1.style.display=="none"){
+     div1.style.display="block";
+     div12.style.display="none";
+    }
+    else {
+    div1.style.display = "none";
+    div12.style.display="block";
+    
+  }
+}
